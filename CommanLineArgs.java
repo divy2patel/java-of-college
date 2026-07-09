ae0d30809f37c1bcbe075666c4fbedc45b30a068
Divy <divy92288@gmail.com>
2026-07-09 17:27:11 +0530

    int addition using command value.

--- CommanLineArgs.java
+++ CommanLineArgs.java
@@ -2,12 +2,25 @@ class CommanLineArgs
 {
 	public static void main(String s[])
 	{
-		int a,b,sum=0;
+		int a=0,b=0,sum=0;
+		/*
 		a=Integer.parseInt(s[0]);
 		b=Integer.parseInt(s[1]); //java CommanLineArgs 5 10 in cmd not forgot or get error
-		
+
 		sum=a+b;
 		
+		System.out.println("Addition is :"+sum);
+		*/
+		if(s.length==2)
+		{
+			a=Integer.parseInt(s[0]);
+			b=Integer.parseInt(s[1]);
+		}
+		else
+		{
+			System.out.println("Enter 2 command value.🥱");
+		}
+		sum=a+b;
 		System.out.println("Addition is :"+sum);
 	}
 }
