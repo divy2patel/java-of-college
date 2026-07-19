0bc476664df40af2800b92acb1736204e4400b82
Divy <divy92288@gmail.com>
2026-07-19 18:08:37 +0530

    GUI in java is awosome🤩

---
+++ WelcomeInDialogbox.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class WelcomeInDialogbox
+{
+    public static void main(String s[])
+    {
+        JOptionPane.showMessageDialog(null, "Welcome to java!");
+    }
+}
