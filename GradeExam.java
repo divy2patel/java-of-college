d9d3bb5bf581af86f077b9e25e412324c5908035
Divy <divy92288@gmail.com>
2026-08-01 09:40:37 +0530

    iwufkjwiu

---
+++ GradeExam.java
@@ -0,0 +1,29 @@
+public class GradeExam {
+/** Main method */
+public static void main(String[] args) {
+// Students’ answers to the questions
+char[][] answers = {
+{'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+{'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+{'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+{'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+{'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+{'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+{'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+{'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}
+};
+char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+for (int i = 0; i < answers.length; i++) {
+ // Grade one student
+ System.out.println(answers.length);
+ int correctCount = 0;
+ for (int j = 0;j < answers[i].length ; j++) {
+    
+ if (answers[i][j] == keys[j])
+ correctCount++;
+ }
+
+ System.out.println("Student " + i + "`s correct count is " +correctCount);
+}
+}
+}
